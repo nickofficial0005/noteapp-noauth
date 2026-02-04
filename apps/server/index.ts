6ce6f496151d5081a65a0c31bc885140d9abc413
Nick <nickofficial0005@gmail.com>
2026-02-04 14:38:27 +0530

    Noteapp setup without Auth

---
+++ apps/server/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+    res.json({message: 'Hello from the Backend'});
+});
+
+const PORT = 5000;
+app.listen(PORT,()=>{
+    console.log(`Server running on http://localhost:${PORT}`);
+
+});
+
